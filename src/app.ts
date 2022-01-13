c5584c0844026f530d2c014fbfd2661117340862
buni <alisson.corrales24@gmail.com>
2022-01-12 18:29:27 -0700

    im so sasd

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+class genMonster {
+    constructor(
+        public id: string,
+        public name: string,
+        public location: string,
+        public challenge: string,
+        public weaknesses: string,
+        public mortality: boolean,
+        public : boolean,
+    ) {}
+}
